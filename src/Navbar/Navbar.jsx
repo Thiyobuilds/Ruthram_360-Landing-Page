c4e78e12adf0e44e4d78da5067838d1570c41360
Thiyobuilds <Thiyo80203@gmail.com>
2025-06-17 12:16:05 -0200

    Ruthram Media Landing Page

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="navbar">
+        <div className="nav-con">
+            <div className="logo"></div>
+            <div className="nav-links"></div>
+            <div className="nav-btn"></div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
